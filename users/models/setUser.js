4ee794e3985f90b76f5237f8f091c94fd58328bc
unknown <murilo001os@gmail.com>
2019-07-18 20:03:44 -0300

    Correções

--- users/models/setUser.js
+++ users/models/setUser.js
@@ -1,13 +1,12 @@
 const db = require('../../repositories/db')
 
 function salvarUsuario(dadosUsuario, callback) {
+	console.log(dadosUsuario);
 	db.run('INSERT INTO usuario(nome, idade, ativo) VALUES(?, ?, ?)', [dadosUsuario.nome, dadosUsuario.idade, dadosUsuario.ativo], (err) => {
 		console.log('changes:' + this.changes);
 		console.log('erro:' + err);
 		if (err) {
 			callback(err);
-		} else if (this.changes == undefined) {
-			callback('Dados inválidos.');
 		} else {
 			callback(null);
 		}
